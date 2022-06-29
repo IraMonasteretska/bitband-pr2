91d1809c982196923aced94a775043fd8a13c77b
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2022-06-29 17:40:44 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -13,14 +13,35 @@ $(document).ready(function () {
     });
 
 
+    // Project Board
+
+    $('.togglebutton').click(function () {
+        $('.boardsidebar').toggleClass('hide');
+        $('.boardrightsect').toggleClass('enlarged');
+    });
 
 
+    // selects
+    if ($('body *').is('select')) {
+        $('.custom-select').select2({
+            placeholder: "Select a state",
+            minimumResultsForSearch: -1,
+        });
+    }
 
+    // sidebar project settmenu
+    $('.projectsgroup__projbox .botsbtn').click(function () {
+        $('.projsettlist').not($(this).parents('.projectsgroup__listwrapper').find('.projsettlist')).removeClass('show');
+        $(this).parents('.projectsgroup__listwrapper').find('.projsettlist').toggleClass('show');
+    });
 
+    $(document).click(function (e) {
+        let $target = $(e.target);
+        if (!$target.closest('.projectsgroup').length) {
+            $('.projsettlist').removeClass('show');
+        }
 
-
-
-
+    });
 
 
 
