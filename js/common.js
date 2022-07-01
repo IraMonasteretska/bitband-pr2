1d532cbe96dbcf33d8f9a42b37cd2d850077b9e2
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2022-07-01 15:10:26 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -112,6 +112,16 @@ $(document).ready(function () {
         });
     }
 
+    $('.custom-select.inmodal2').select2({
+        placeholder: "Select a state",
+        minimumResultsForSearch: -1,
+        dropdownParent: $("#taskmodal"),
+    });
+
+
+
+
+
     // team - role btn
 
     $('.teamroles span').click(function () {
@@ -128,26 +138,90 @@ $(document).ready(function () {
     });
 
 
-    // test
-
-    $('.test').click(function () {
-        var divList = $(".div");
-        divList.sort(function (a, b) {
-            return String.prototype.localeCompare.call($(a).data('name').toLowerCase(), $(b).data('name').toLowerCase());
-        });
-        $("#list-of-divs").html(divList);
+    // sortbyname
+    $('.sortbyname').click(function () {
+        sortTable(0)
     });
 
 
+    function sortTable(n) {
+        var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+        table = document.getElementById("myTable");
+        switching = true;
+        //Set the sorting direction to ascending:
+        dir = "asc";
+        /*Make a loop that will continue until
+        no switching has been done:*/
+        while (switching) {
+            //start by saying: no switching is done:
+            switching = false;
+            rows = table.getElementsByTagName("TR");
+            /*Loop through all table rows (except the
+            first, which contains table headers):*/
+            for (i = 1; i < (rows.length - 1); i++) {
+                //start by saying there should be no switching:
+                shouldSwitch = false;
+                /*Get the two elements you want to compare,
+                one from current row and one from the next:*/
+                x = rows[i].getElementsByTagName("TD")[n];
+                y = rows[i + 1].getElementsByTagName("TD")[n];
+                /*check if the two rows should switch place,
+                based on the direction, asc or desc:*/
+                if (dir == "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                } else if (dir == "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                /*If a switch has been marked, make the switch
+                and mark that a switch has been done:*/
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                //Each time a switch is done, increase this count by 1:
+                switchcount++;
+            } else {
+                /*If no switching has been done AND the direction is "asc",
+                set the direction to "desc" and run the while loop again.*/
+                if (switchcount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                }
+            }
+        }
+    }
 
 
+    // OPAN TASK
+    $('.toggleopentask').click(function () {
+        $(this).toggleClass('rotate');
+        $('.taskmodal__content').toggleClass('big');
+        $('.taskmodal__details').toggleClass('hide');
+    });
 
+    $('.taskrole__selected').click(function () {
+        $('.taskrole__selected').not($(this)).removeClass('active');
+        $('.taskrole__userlist').not($(this).next('.taskrole__userlist')).hide();
 
+        $(this).toggleClass('active');
+        $(this).next('.taskrole__userlist').toggle();
+    });
 
-
-
-
-
+    $(document).click(function (e) {
+        let $target = $(e.target);
+        if (!$target.closest('.taskrole').length) {
+            $('.taskrole__selected').removeClass('active');
+            $('.taskrole__userlist').hide();
+        }
+    });
 
 
 });
